7d56923a301212dec48cc9b925e664b69d0a2414
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-02-28 10:56:45 -0300

    Update 01_getVectors.js

--- 01_getVectors.js
+++ 01_getVectors.js
@@ -1,2 +1,18 @@
 // get time-series of degradation vectors and compute statistical summaries
 // dhemerson.costa@ipam.org.br (ipam and mapbiomas brazil) 
+
+// fire
+var fire = ee.Image('projects/mapbiomas-workspace/public/collection7/mapbiomas-fire-collection1-1-annual-burned-coverage-1');
+var fire_freq = ee.Image('projects/mapbiomas-workspace/public/collection7/mapbiomas-fire-collection1-1-fire-frequency-1');
+fire_freq = fire_freq.select(['fire_frequency_1985_2021']).divide(100).int();
+
+Map.addLayer(fire_freq.randomVisualizer());
+
+// lcluc change
+
+
+// deforestation 
+
+
+// climatic
+

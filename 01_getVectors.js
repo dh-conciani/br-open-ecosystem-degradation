840abc41dab95c15fbfe5ac795790479d7ddb006
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-03-07 14:42:36 -0300

    Update 01_getVectors.js

--- 01_getVectors.js
+++ 01_getVectors.js
@@ -1,4 +1,4 @@
-// get disturbance frequency based on mapbiomas products 
+// get time-series of degradation vectors and compute statistical summaries
 // dhemerson.costa@ipam.org.br (ipam and mapbiomas brazil) 
 
 // get mapbiomas land cover
@@ -12,21 +12,26 @@ var mapbiomas_native = mapbiomas.remap({
   defaultValue: 0})
   .rename('native_vegetation');
 
-// get fire frequency from mapbiomas fogo (col 1)
-var fire_freq = ee.Image('projects/mapbiomas-workspace/public/collection7/mapbiomas-fire-collection1-1-fire-frequency-1')
-  .select(['fire_frequency_1985_2021']).divide(100).int()
-  .updateMask(mapbiomas_native.neq(0))
+// 
+var fill_map = function(image) {
+  return image.updateMask(mapbiomas_native.neq(0))
   .unmask(0)
-  .updateMask(mapbiomas)
-  .rename('fire_freq');
+  .updateMask(mapbiomas);
+};
+
+// get fire frequency from mapbiomas fogo (col 1)
+var fire_freq = fill_map(
+  ee.Image('projects/mapbiomas-workspace/public/collection7/mapbiomas-fire-collection1-1-fire-frequency-1')
+  .select(['fire_frequency_1985_2021']).divide(100).int()
+  .rename('fire_freq')
+  );
 
 // get deforestation frequency from mapbiomas lcluc (col 7.1)
-var deforestation = ee.Image('projects/mapbiomas-workspace/public/collection7_1/mapbiomas_collection71_deforestation_frequency_v1')
+var deforestation = fill_map(
+  ee.Image('projects/mapbiomas-workspace/public/collection7_1/mapbiomas_collection71_deforestation_frequency_v1')
   .select(['desmatamento_frequencia_1987_2020']).divide(100).int()
-  .updateMask(mapbiomas_native.neq(0))
-  .unmask(0)
-  .updateMask(mapbiomas)
-  .rename('deforestation_freq');
+  .rename('deforestation_freq')
+  );
 
 // years as anthropogenic use
 

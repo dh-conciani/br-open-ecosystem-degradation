1c01f11e511deb535c13b36eb91487d396e52d8a
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-03-08 16:39:30 -0300

    Update 02_getNomalizationParams.js

--- 02_getNomalizationParams.js
+++ 02_getNomalizationParams.js
@@ -4,6 +4,8 @@
 // insert metadata
 var root = 'projects/mapbiomas-workspace/DEGRADACAO/DISTURBIOS/disturbance_frequency/brazil_disturbance_frequency_';
 var input_version = 2;
+
+var output = 'projects/mapbiomas-workspace/DEGRADACAO/DISTURBIOS/PARAMETROS/DISTURBANCE_FREQUENCY';
 var output_version = 2;
 
 // read distubance database 
@@ -14,43 +16,48 @@ var mapbiomas = ee.Image('projects/mapbiomas-workspace/public/collection7/mapbio
   .select(['classification_2021']);
 
 // set classes in which parameters will be estimated (only native vegetation)
-//var classes = [3, 4, 5, 11, 12, 13, 32, 49, 50];
-var classes = [4, 12];
+var classes = [3, 4, 5, 11, 12, 13, 32, 49, 50];
+//var classes = [4, 12];
 
 // read normalization territory (biomes)
-var territory = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biomas-2019').first();
+var territory = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biomas-2019');
 
 
-// set funciton to get the maximum value to be used in [range] normalization 
+// set function to get the maximum value to be used in [range] normalization 
 var getParams = function(image, feature) {
-  // get data for the class i
-  var class_i = disturbance.updateMask(mapbiomas.eq(12));
-
-  // get maximum
-  var max_values = class_i.rename(['anthropogenic_freq_max', 'deforestation_freq_max', 'fire_freq_max', 'sum_of_disturbance_max'])
-    .reduceRegion({
-      reducer: ee.Reducer.max(),
-      geometry: feature.geometry(),
-      scale: 30,
-      maxPixels: 1e13
+  var class_params = classes.map(function(class_j) {
+    var disturbance_ij = disturbance.updateMask(mapbiomas.eq(class_j));
+    // get maximum value 
+    var max_values = disturbance_ij.rename(['anthropogenic_freq_max', 'deforestation_freq_max', 'fire_freq_max', 'sum_of_disturbance_max'])
+      .reduceRegion({
+        reducer: ee.Reducer.max(),
+        geometry: feature.geometry(),
+        scale: 30,
+        maxPixels: 1e13
+    });
+    // get miminum
+    var min_values = disturbance_ij.rename(['anthropogenic_freq_min', 'deforestation_freq_min', 'fire_freq_min', 'sum_of_disturbance_min'])
+      .reduceRegion({
+        reducer: ee.Reducer.min(),
+        geometry: feature.geometry(),
+        scale: 30,
+        maxPixels: 1e13
+    });
+    // return params for the class [j]
+     return feature.set(max_values)
+                   .set(min_values)
+                   .set('class_id', class_j);
+                
   });
-  
-  // get miminum
-  var min_values = class_i.rename(['anthropogenic_freq_min', 'deforestation_freq_min', 'fire_freq_min', 'sum_of_disturbance_min'])
-    .reduceRegion({
-      reducer: ee.Reducer.min(),
-      geometry: feature.geometry(),
-      scale: 30,
-      maxPixels: 1e13
-  });
-  
-  return feature.set(max_values)
-                .set(min_values)
-                .set('class_id', 12);
-  
+  // return params for the feature [i]
+  return ee.FeatureCollection(class_params).flatten();
 };
 
+// compute params
+var params = getParams(disturbance, territory);
+print(params);
 
-Map.addLayer(territory.geometry())
-var x = getParams(disturbance, territory);
-print(x)
+// export as GEE asset
+Export.table.toAsset({'collection': params, 
+                      'description': 'params_disturbance_frequency_class_biome_v' + output_version,
+                      'assetId': output + '/' + 'params_disturbance_frequency_class_biome_v' + output_version});

45e8c537d93db05e66ae4cc70dedfebed8dbed61
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-03-08 15:50:33 -0300

    Update 02_getNomalizationParams.js

--- 02_getNomalizationParams.js
+++ 02_getNomalizationParams.js
@@ -23,8 +23,12 @@ var territory = ee.FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biom
 
 // set funciton to get the maximum value to be used in [range] normalization 
 var getParams = function(image, feature) {
+  // get data for the class i
+  var class_i = disturbance.where(mapbiomas.eq(4), disturbance);
+  Map.addLayer(class_i.randomVisualizer());
+
   // get maximum
-  var max_values = image.rename(['anthropogenic_freq_max', 'deforestation_freq_max', 'fire_freq_max', 'sum_of_disturbance_max'])
+  var max_values = class_i.rename(['anthropogenic_freq_max', 'deforestation_freq_max', 'fire_freq_max', 'sum_of_disturbance_max'])
     .reduceRegion({
       reducer: ee.Reducer.max(),
       geometry: feature.geometry(),
@@ -33,9 +37,9 @@ var getParams = function(image, feature) {
   });
   
   // get miminum
-  var min_values = image.rename(['anthropogenic_freq_min', 'deforestation_freq_min', 'fire_freq_min', 'sum_of_disturbance_min'])
+  var min_values = class_i.rename(['anthropogenic_freq_min', 'deforestation_freq_min', 'fire_freq_min', 'sum_of_disturbance_min'])
     .reduceRegion({
-      reducer: ee.Reducer.max(),
+      reducer: ee.Reducer.min(),
       geometry: feature.geometry(),
       scale: 30,
       maxPixels: 1e13

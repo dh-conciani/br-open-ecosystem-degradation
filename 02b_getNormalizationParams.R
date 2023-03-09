3c151609a5f92fa1d535af78fcc77fe8403439d1
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-03-09 09:17:46 -0300

    Create 02b_getNormalizationParams.R

---
+++ 02b_getNormalizationParams.R
@@ -0,0 +1,53 @@
+## get noralization parameters (min-max) for each variable in function of class~biome
+## dhemerson.costa@ipam.org.br
+
+## read libraries
+library(rgee)
+ee_Initialize()
+
+## read distubance database 
+disturbance <- ee$Image('projects/mapbiomas-workspace/DEGRADACAO/DISTURBIOS/disturbance_frequency/brazil_disturbance_frequency_2');
+
+## read mapbiomas collection 7 in the last year
+mapbiomas <- ee$Image('projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2')$
+  select('classification_2021')
+
+## set classes in which parameters will be estimated (only native vegetation)
+classes = c(3, 4, 5, 11, 12, 13, 32, 49, 50)
+
+## read normalization territory (biomes)
+territory <- ee$FeatureCollection('projects/mapbiomas-workspace/AUXILIAR/biomas-2019')
+
+## get territories ID
+territory_list <- territory$aggregate_array('Bioma')$getInfo()
+
+## for each territory
+for (i in 1:length(unique(territory_list))) {
+  ## get territory [i]
+  territory_i <- territory$filterMetadata('Bioma', 'equals', territory_list[i])
+  ## for each class [j]
+  for (j in 1:length(unique(classes))) {
+    ## get disturbance data [ij] 
+    disturbance_ij = disturbance$
+      clip(territory_i)$
+      updateMask(mapbiomas$eq(classes[j]))
+    
+    
+    
+  }
+}
+
+
+
+## get territory [i]
+territory_i <- territory$filterMetadata('Bioma', 'equals', territory_list[1])
+
+## for each class [j]
+
+  ## get disturbance data [ij] 
+disturbance_ij = disturbance$
+  clip(territory_i)$
+  updateMask(mapbiomas$eq(classes[1]))
+
+Map$addLayer(disturbance_ij)  
+  

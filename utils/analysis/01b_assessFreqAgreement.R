8e40aa6cf85dbe3846754e0ef76e1cbdc8ac8501
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-03-13 17:04:26 -0300

    Update 01b_assessFreqAgreement.R

--- utils/analysis/01b_assessFreqAgreement.R
+++ utils/analysis/01b_assessFreqAgreement.R
@@ -169,13 +169,14 @@ ggplot(data= legend_x, mapping=aes(x= ecosystem, y= area/1e6, fill= disturbance)
   geom_bar(stat = "identity", position= position_fill(0.5), width= 99) +
   geom_text_repel(mapping=aes(x= ecosystem, y= area/1e6,
                         label= paste0(round(perc, digits=0), '%')), 
-            position=position_fill(0.5), size=6) +
+            position=position_fill(0.5), size=4) +
   coord_polar(theta='y') +
   theme_minimal() +
   scale_fill_manual('Disturbance', values=c('red', '#00F318', '#EF9A2C', '#529CA8', '#FFEC33', '#20F0E2', '#606060', '#C0C0C0')) +
-  facet_wrap(~ecosystem) +
+  facet_wrap(~ecosystem, ncol=3) +
   theme_void() +
-  theme(text = element_text(size = 20)) 
+  theme(text = element_text(size = 20))  +
+  theme(legend.position = "none")
 
 ## export for data studio
 #write.csv(data_l0i, './exports/disturbance/disturbance_per_biome.csv')
@@ -211,17 +212,106 @@ data_l1i$disturbance_str <- factor(data_l1i$disturbance_str,
                                               "No disturbance"
                                    ))
 
-## plot specific
-ggplot(data= subset(data_l1i, biome_str == 'Cerrado'), mapping= aes(x= class_str, y= per_class_biome_perc, fill= disturbance_str)) +
-  geom_bar(stat='identity', position= 'stack', alpha= 0.8) +
+
+## aggregate for brazil
+x1 <- aggregate(x=list(area= data_l1i$area), 
+          by= list(disturbance= data_l1i$disturbance_str,
+                   class= data_l1i$class_str,
+                   ecosystem= data_l1i$ecosystem_str), FUN= 'sum')
+
+## get relatives
+for (i in 1:length(unique(x1$class))) {
+  x <- subset(x1, class== unique(x1$class)[i])
+  x$perc <- round(x$area/sum(x$area)*100, digits=1)
+  x$perc_non <- subset(x, disturbance == 'No disturbance')$perc
+  if(exists('x2') == FALSE) {
+    x2 <- x
+  } else {
+    x2 <- rbind(x2, x)
+  }
+}; rm(x)
+
+## reorder ecosystem
+x2$ecosystem <- factor(x2$ecosystem, 
+                           levels = c("Forest ecosystem",
+                                      "Non-forest ecosystem",
+                                      "Coastal ecosystem"
+                           ))
+
+## plot classes
+ggplot(data=x2, mapping=aes(x= reorder(class, perc_non), y= perc, fill= disturbance)) +
+  geom_bar(stat='identity', position='stack') +
   scale_fill_manual('Disturbance', values=c('red', '#00F318', '#EF9A2C', '#529CA8', '#FFEC33', '#20F0E2', '#606060', '#C0C0C0')) +
-  #facet_grid(class_str~biome_str, scales= 'free_x') +
-  geom_text_repel(mapping=aes(label=paste0(per_class_biome_perc, '%')), position=position_stack(0.5), size= 2.5) +
-  theme_bw() +
-  theme(strip.text.y = element_text(size = 7))
+  facet_wrap(~ecosystem, 'free', nrow=3, ncol=3) +
+  theme_minimal() +
+  geom_text_repel(mapping=aes(label=perc), position=position_stack(0.5)) +
+  theme(text = element_text(size = 14)) +
+  theme(legend.position = "none")
+
+
+## reodrder
+data$disturbance_str <- factor(data$disturbance_str, 
+                                   levels = c("Fire + Veg. loss + Anthropogenic use",
+                                              "Veg. loss + Anthropogenic use",
+                                              "Fire + Anthropogenic use",
+                                              "Fire + Veg. loss",
+                                              "Anthropogenic use",
+                                              "Veg. loss",
+                                              "Fire",
+                                              "No disturbance"
+                                   ))
+
+
+## compute percents
+for (i in 1:length(unique(data$ecosystem_str))) {
+  x <- subset(data, ecosystem_str == unique(ecosystem_str)[i])
+  x <- aggregate(x=list(area= x$area), by= list(biome= x$biome_str,
+                                                disturbance= x$disturbance_str,
+                                                ecosystem= x$ecosystem_str),
+                 FUN= 'sum')
+  
+  for (j in 1:length(unique(x$biome))) {
+    y <- subset(x, biome == unique(x$biome)[j])
+    y$perc <- round(y$area/sum(y$area)*100, digits=1)
+    if(exists('x3') == FALSE) {
+      x3 <- y
+    } else {
+      x3 <- rbind(x3, y)
+    }
+  }
+}rm(y)
+
+## reorder ecosystem
+x3$ecosystem <- factor(x3$ecosystem, 
+                       levels = c("Forest ecosystem",
+                                  "Non-forest ecosystem",
+                                  "Coastal ecosystem"
+                       ))
+
+## comparar biomas
+ggplot(data= x3, mapping= aes(x= biome, y= perc, fill= disturbance)) +
+  geom_bar(stat='identity') +
+  facet_wrap(~ecosystem, scales= 'fixed') +
+  scale_fill_manual('Disturbance', values=c('red', '#00F318', '#EF9A2C', '#529CA8', '#FFEC33', '#20F0E2', '#606060', '#C0C0C0')) +
+  coord_flip() +
+  theme_minimal() +
+  theme(text = element_text(size = 20)) +
+  theme(legend.position = "none") +
+  geom_text_repel(mapping=aes(label=perc), position=position_stack(0.5))
+  
+
+
+## plot specific
+#ggplot(data= data_l1i, mapping= aes(x= class_str, y= per_class_biome_perc, fill= disturbance_str)) +
+#  geom_bar(stat='identity', position= 'stack', alpha= 0.8) +
+#  scale_fill_manual('Disturbance', values=c('red', '#00F318', '#EF9A2C', '#529CA8', '#FFEC33', '#20F0E2', '#606060', '#C0C0C0')) +
+#  facet_grid(class_str~biome_str, scales= 'free_x') +
+#  geom_text_repel(mapping=aes(label=paste0(per_class_biome_perc, '%')), position=position_stack(0.5), size= 2.5) +
+#  theme_bw() +
+#  theme(strip.text.y = element_text(size = 7))
 
 ## export to data-studio
-write.csv(data_l1i, './exports/disturbance/disturbance_per_class_biome.csv')
+#write.csv(data_l1i, './exports/disturbance/disturbance_per_class_biome.csv')
 
 ## cores de legenda
 hex <- 
@@ -248,4 +338,5 @@ hex <-
       )
   )
 
-#write.csv(hex, './exports/disturbance/hex_values.csv', row.names=FALSE)
+
+write.csv(data, './exports/disturbance/data_disturbance_v1.csv', row.names=FALSE)

9d3fd6d1ae329dfea6a598a07e78aec72783ffd7
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-04-05 10:25:21 -0300

    Update 04b_stateChange.R

--- 04b_stateChange.R
+++ 04b_stateChange.R
@@ -3,7 +3,14 @@
 
 ## import libraries
 library(rgee)
+library(stringr)
 ee_Initialize()
+
+## set native classes
+native_classes = c(3, 4, 11, 12)
+
+## set ignored classes
+ignore_classes = c(33, 27)
 
 ## get collection
 collection <- ee$Image('projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2')
@@ -16,6 +23,7 @@ grid_ids <- unique(grid$aggregate_array('id')$getInfo())
 
 ## for each carta
 for (i in 1:length(grid_list)) {
+  print(paste0('processing tile ', i, ' of ', length(grid_ids)))
   ## get carta [i]
   grid_i <- grid$filterMetadata('id', 'equals', grid_ids[i])
   
@@ -29,16 +37,22 @@ for (i in 1:length(grid_list)) {
   collection_i_arr <- ee_as_sf(collection_i, via= 'drive')
   
   ## get only pixels that was native vegetation in the last year (2021)
-  trajs <- subset(collection_i_arr, classification_2021 == 3 |
-                                    classification_2021 == 4 |
-                                    classification_2021 == 11 |
-                                    classification_2021 == 12 |
-                                    classification_2021 == 33)
+  trajs_nv_ly <- subset(collection_i_arr, classification_2021 == 3 |
+                                          classification_2021 == 4 |
+                                          classification_2021 == 11 |
+                                          classification_2021 == 12 |
+                                          classification_2021 == 33 |
+                                          classification_2021 == 27)
   
-  ## for each pixel [id]
   
-  ### get trajectory
-  traj_i <- collection_i_arr[1,]
+ ## for each pixel
+  for (j in 1:length(unique(trajs_nv_ly$id))) {
+    print(paste0('processing pixel ', j, ' of ', length(unique(trajs_nv_ly$id))))
+    ## get pixel i
+    pixel_ij <- subset(trajs_nv_ly, id == unique(trajs_nv_ly$id)[j])
+          
+  }
+  
   
   
   
@@ -47,4 +61,3 @@ for (i in 1:length(grid_list)) {
   
 }
 
-print()

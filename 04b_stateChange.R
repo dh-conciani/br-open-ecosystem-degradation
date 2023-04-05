07931f0ab379762e55191179260bfde7fba3475c
Dhemerson E. Conciani <dhemerson_conciani@yahoo.com.br>
2023-04-05 15:08:46 -0300

    Update 04b_stateChange.R

--- 04b_stateChange.R
+++ 04b_stateChange.R
@@ -12,56 +12,39 @@ native_classes <- c(3, 4, 11, 12, 33, 27)
 ## set persistence rule threshold (in years)
 persistence <- 2
 
-## set ignored classes
-##ignore_classes <- c(33, 27)
-
 ## set years
 years_list <- seq(1985, 2021)
 
 ## get collection
 collection <- ee$Image('projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2')
 
-## remmap native vegetation to 1
-for(i in 1:length(years_list)) {
-  ## read year i
-  collection_y <- collection$select(paste0('classification_', years_list[i]))
-  ## remap
-  collection_y <- collection_y$remap(
+## get last year classification
+collection_last <- collection$select('classification_2021')$
+  ## and remmap to retain only native vegetation
+  remap(
     from= native_classes,
     to= rep(1, length(native_classes)),
-    defaultValue= 0
-  )
-  
-  ## store
-  if (exists('remmaped_nv') == FALSE) {
-    remmaped_nv <- collection_y$rename(paste0('classification_', years_list[i]))
-  } else {
-    remmaped_nv <- remmaped_nv$addBands(collection_y$rename(paste0('classification_', years_list[i])))
-  }
-  
-}
+    defaultValue= 0)$
+  selfMask()
 
-## get the number of classes
-nClasses <- remmaped_nv$reduce(ee$Reducer$countDistinctNonNull())
+## clip the entire collection for the native vegetation of the last year
+collection <- collection$updateMask(collection_last)
 
-## get only stable as NV 
-stable <- remmaped_nv$select(0)$multiply(nClasses$eq(1))
+## To simplify the local API processing, remove the 100% stable native vegetation
+## For this, compute the number of classes in the time-series 
+nClasses <- collection$reduce(ee$Reducer$countDistinctNonNull())
 
-## filter the entire collection only for stable NV
-collection <- collection$updateMask(stable$eq(1))
+## And mask the collection to get only native vegetation that occurs in 2021 and is unstable over the time-series
+collection <- collection$updateMask(nClasses$neq(1))
 
-## check stability in the level-3 and retain only patches of NV that changed of class
-nClasses3 <- collection$reduce(ee$Reducer$countDistinctNonNull())
+## Inspect
+Map$addLayer(nClasses$randomVisualizer(), {}, 'Number of classes') +
+  Map$addLayer(collection2$randomVisualizer(), {}, 'Unstable native vegetation')
 
-## updateCollection only with NV that have changed
-collection <- collection$updateMask(nClasses3$neq(1))
-
-Map$addLayer(collection$select(0)$randomVisualizer(), {}, 'Stable NV that changed class')
-
-## get grid list
+## Trajectory assessment is too complex. For this, we used a regular tile of 25 x 25 km approach 
 grid <- ee$FeatureCollection('users/dh-conciani/basemaps/br_grid_25_x_25_km')
 
-## get cartas name
+## Get tile label
 grid_ids <- unique(grid$aggregate_array('id')$getInfo())
 
 ## for each carta
@@ -79,7 +62,6 @@ for (i in 1:length(grid_ids)) {
   ## get locally
   collection_i_arr <- ee_as_sf(collection_i, via= 'drive')
   
-
   ## for each pixel
   for (j in 1:length(unique(collection_i_arr$id))) {
     print(paste0('processing pixel ', j, ' of ', length(unique(collection_i_arr$id))))
@@ -99,7 +81,6 @@ for (i in 1:length(grid_ids)) {
     }
     
     ## Now, pixel_ij contains the 'barcode' of trajectory
-    
     ## Extract trajectory classes, preserve ordering and get their respective frequency
     traj_res <- as.data.frame(cbind(
                   value= rle(list_ij)$values,
@@ -107,6 +88,14 @@ for (i in 1:length(grid_ids)) {
     
     ## Discard segments with less than 2 years in the trajectory
     traj_res <- subset(traj_res, length > persistence)
+    
+    ## HERE IS PLACED THE RULES 
+    
+    ## Rule 1: Temporary vs. persistent changes 
+    ## TEMPORARY:
+    ## IF START CLASS IS EQUALS TO END CLASS (FILTERED BY 2 YEAR STABILITY) THE CHANGE WAS TEMPORARY
+    ## PERSISTENT:
+    ## IF END CLASS IS DIFFERENT OF THE START CLASS (FILTERED BY 2 YEARS STABILITY) THE CHANGE WAS PERSISTENT
     
     ## If start and final classes are the same, compute as a temporary change
     if (traj_res$value[1] == traj_res$value[nrow(traj_res)]) {

c7daec4e76685086c0034d979d00b0a0b7b82771
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2023-07-03 17:06:50 -0300

    Update 03a_natVeg_stateChange_qualify.R

--- 03a_natVeg_stateChange_qualify.R
+++ 03a_natVeg_stateChange_qualify.R
@@ -1,1 +1,12 @@
+## qualify state change among enchroachment or thinning
+## dhemerson.costa@ipam.org.br
+## gt degradação mapbiomas
+
+## read libraries
+library(rgee)
+
+## authenticate
+ee_Initialize(drive= TRUE,
+              gcs= TRUE)
+
 

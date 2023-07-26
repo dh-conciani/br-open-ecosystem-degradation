22c57783324cd86d7a5e51c5ebcc0fa01394787f
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2023-07-26 10:30:26 -0300

    Update 03a_natVeg_stateChange_qualify.R

--- 03a_natVeg_stateChange_qualify.R
+++ 03a_natVeg_stateChange_qualify.R
@@ -4,19 +4,59 @@
 
 ## read libraries
 library(rgee)
+library(stringr)
+library(sf)
+library(dplyr)
+library(googleCloudStorageR)
+library(raster)
 
 ## authenticate
 ee_Initialize(drive= TRUE,
               gcs= TRUE)
+
+## Set classes to be considered in the trajectory analisys
+native_classes <- c(3, 4, 11, 12)
+native_classes_adjusted <- c(3, 4, 12, 12)
 
 ## read entry layer (state change in the level-0)
 state_change <- ee$Image('projects/mapbiomas-workspace/DEGRADACAO/TRAJECTORIES/COL71/NV_CHANGE_V3')$
   remap(from= c(1, 2, 3, 4, 5),
         to=   c(2, 3, 4, 5, 1))
 
+## Set classes to be used in the state change analisys
+assess_classes <- c(3, 4, 12) ## 11 (wetland will be remmaped to grassland)
+
+## set years
+years_list <- seq(1985, 2021)
+
+## get collection
+collection <- ee$Image('projects/mapbiomas-workspace/public/collection7/mapbiomas_collection70_integration_v2')
+
+## remap to consider grassland and wetland as a unique class (id 12)
+years_to_remap <- sub(".*_", "", collection$bandNames()$getInfo())
+collection_x <- ee$Image() ## build an empty recipe 
+for (i in 1:length(years_to_remap)) {
+  ## get collection for the year i
+  x <- collection$select(paste0('classification_', years_to_remap[i]))
+  ## remap
+  x <- x$remap(
+    from= native_classes,
+    to= native_classes_adjusted,
+    defaultValue= 0
+  )$rename(paste0('classification_', years_to_remap[i]))
+  
+  ## bind
+  collection_x <- collection_x$addBands(x)
+} 
+
+print(collection_x$bandNames()$getInfo())
+Map$addLayer(collection_x)
+
+
+
+
 Map$addLayer(state_change, list(palette=c('#2D7E1D', '#75F70A', '#606060', '#FFF700', '#F41BE7'),
                                 min=1, max=5), 'NV state change')
 
 
 
-z

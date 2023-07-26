991ff549f54a010cab3b4f1cc85d5eb42792378e
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2023-07-26 11:04:27 -0300

    Update 03a_natVeg_stateChange_qualify.R

--- 03a_natVeg_stateChange_qualify.R
+++ 03a_natVeg_stateChange_qualify.R
@@ -84,8 +84,94 @@ if(length(processed_with_letters) > 0) {
   grid_ids <- grid_ids[-which(grid_ids %in% processed_with_letters)]
 }
 
+## for each carta 
+for (i in 1:length(grid_ids)) {
+  print(paste0('processing tile ', i, ' of ', length(grid_ids)))
+  ## Get carta [i]
+  grid_i <- grid$filterMetadata('id', 'equals', grid_ids[i])
+  
+  ## check if the grid is already processed and, if true, skip iteration
+  if (grid_ids[i] %in% ee$ImageCollection(out_dir)$aggregate_array('system:index')$getInfo() == TRUE) {
+    next
+  }
+  
+  ## Get pixel values
+  collection_i <- collection$sample(region= grid_i$geometry(), 
+                                    scale = 30,
+                                    geometries= TRUE,
+                                    tileScale= 16)
+  
+  # Call a function that might throw an error
+  print('Downloading sampled results')
+  result <- try({
+    collection_i_arr <- ee_as_sf(collection_i, via = "drive", quiet= TRUE)
+  }, silent = TRUE)
 
-
+  # Check if there was an error
+  if (inherits(result, "try-error")) {
+    # Extract the error message from the result object
+    result <- as.character(result)
+  }
+  print('Download OK!')
+  
+  ## HERE STARTS THE RULES TO DEAL WITH THE ERRORS
+  ## RULE 1: And it is empty (full NA), export a full-NA grid
+  if(grepl("The source could be corrupt or not supported", result) == TRUE) {
+    # Do something if the text is found
+    print('Downloading tile to export as NA')
+    
+    # Define the raster extent and resolution as the same of the grid
+    r <- raster(extent(ee_as_sf(grid_i, via = 'drive')), resolution = 0.0002694946 )
+    ## Set all pixels to NA
+    r <- setValues(r, NA)
+    
+    # Set the projection to EPSG 4326
+    r@crs <- CRS("+init=EPSG:4326")
+    proj4string(r) <- CRS("+proj=longlat +datum=WGS84")
+    
+    ## Export to GEE
+    raster_as_ee(
+      x = r,
+      overwrite = TRUE,
+      assetId = paste0(out_dir, '/', grid_ids[i]),
+      bucket = "degrad-traj1"
+    )
+    
+    rm(r, result)
+    gc()
+    next
+  } 
+  
+  print('Getting trajectories')
+  # Convert the data.frame to a list where each row is an independent sublist
+  lst <- apply(collection_i_arr, 1, as.list)
+  # Remove the first (id) and last (geometry) entries of each sublist
+  lst_x <- lapply(lst, function(x) x[2:(length(x)-1)])
+  ## Get trajectories as lists
+  trajs <- lapply(lst_x, function(pixel) c(
+    pixel$classification_1985, pixel$classification_1986, pixel$classification_1987, 
+    pixel$classification_1988, pixel$classification_1989, pixel$classification_1990, 
+    pixel$classification_1991, pixel$classification_1992, pixel$classification_1993,
+    pixel$classification_1994, pixel$classification_1995, pixel$classification_1996, 
+    pixel$classification_1997, pixel$classification_1998, pixel$classification_1999,
+    pixel$classification_2000, pixel$classification_2001, pixel$classification_2002,
+    pixel$classification_2003, pixel$classification_2004, pixel$classification_2005,
+    pixel$classification_2006, pixel$classification_2007, pixel$classification_2008,
+    pixel$classification_2009, pixel$classification_2010, pixel$classification_2011,
+    pixel$classification_2012, pixel$classification_2013, pixel$classification_2014,
+    pixel$classification_2015, pixel$classification_2016, pixel$classification_2017,
+    pixel$classification_2018, pixel$classification_2019, pixel$classification_2020,
+    pixel$classification_2021))
+  
+  ## Compute Run Length Encoding
+  traj_rle <- lapply(trajs, function(pixel) as.data.frame(cbind(
+    value= rle(pixel)$values,
+    length= rle(pixel)$lengths))) 
+  
+  ## qualify trajectories
+  ## SEGMENT 1: WOODY ENCHROACHMENT
+  
+}
 
 Map$addLayer(state_change, list(palette=c('#2D7E1D', '#75F70A', '#606060', '#FFF700', '#F41BE7'),
                                 min=1, max=5), 'NV state change') +

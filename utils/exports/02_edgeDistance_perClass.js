e8d58031c35234e5d8e90975573b7cf23a05bd69
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2023-10-04 11:07:00 -0300

    Update 02_edgeDistance_perClass.js

--- utils/exports/02_edgeDistance_perClass.js
+++ utils/exports/02_edgeDistance_perClass.js
@@ -5,9 +5,10 @@
 // calculate area of @author João Siqueira
 
 // set input image collection
-var native_edge = ee.ImageCollection('projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/edge_area')
-  // filter version
-  .filterMetadata('version', 'equals', 1);
+var native_edge = ee.ImageCollection('projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/edge_area');
+
+// set version
+var version = 1;
 
 // define years to be computed
 var bands = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
@@ -38,78 +39,79 @@ var recipe = ee.FeatureCollection([]);
 distances.forEach(function(distance_i) {
   
   // filter image
-  var image_i = native_edge.filterMetadata('distance', 'equals', distance_i).min();
+  var image_i = native_edge
+    .filterMetadata('version', 'equals', version)
+    .filterMetadata('distance', 'equals', distance_i)
+    .min();
 
-      // Geometry to export
-      var geometry = native_edge.geometry();
+  // Geometry to export
+  var geometry = native_edge.geometry();
       
-      // convert a complex object to a simple feature collection 
-        var convert2table = function (obj) {
-          obj = ee.Dictionary(obj);
-            var territory = obj.get('territory');
-            var classesAndAreas = ee.List(obj.get('groups'));
-            
-            var tableRows = classesAndAreas.map(
-                function (classAndArea) {
-                    classAndArea = ee.Dictionary(classAndArea);
-                    var classId = classAndArea.get('class');
-                    var area = classAndArea.get('sum');
-                    var tableColumns = ee.Feature(null)
-                        .set('ecoregion', territory)
-                        .set('class', class_i)
-                        .set('value', classId)
-                        .set('area', area)
-                    return tableColumns;
-                }
-            );
+  // convert a complex object to a simple feature collection 
+    var convert2table = function (obj) {
+      obj = ee.Dictionary(obj);
+        var territory = obj.get('territory');
+        var classesAndAreas = ee.List(obj.get('groups'));
         
-            return ee.FeatureCollection(ee.List(tableRows));
-        };
-        
-        // compute the area
-        var calculateArea = function (image, territory, geometry) {
-            var territotiesData = pixelArea.addBands(territory).addBands(image)
-                .reduceRegion({
-                    reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
-                    geometry: geometry,
-                    scale: scale,
-                    maxPixels: 1e12
-                });
-                
-            territotiesData = ee.List(territotiesData.get('groups'));
-            var areas = territotiesData.map(convert2table);
-            areas = ee.FeatureCollection(areas).flatten();
-            return areas;
-        };
-        
-      // perform per year 
-        var areas = bands.map(
-            function (band_i) {
-                var image = structural_change.select('classification_' + band_i)
-                  // filter only to class [i]
-                  .updateMask(collection.select('classification_' + band_i).eq(class_i));
-                  
-                var areas = calculateArea(image, territory, geometry);
-                // set additional properties
-                areas = areas.map(
-                    function (feature) {
-                        return feature.set('variable', band_i);
-                    }
-                );
-                return areas;
+        var tableRows = classesAndAreas.map(
+            function (classAndArea) {
+                classAndArea = ee.Dictionary(classAndArea);
+                var classId = classAndArea.get('class');
+                var area = classAndArea.get('sum');
+                var tableColumns = ee.Feature(null)
+                    .set('ecoregion', territory)
+                    .set('class', classId)
+                    .set('distance', distance_i)
+                    .set('area', area);
+                return tableColumns;
             }
         );
-        
-        // apply function
+    
+        return ee.FeatureCollection(ee.List(tableRows));
+    };
+    
+    // compute the area
+    var calculateArea = function (image, territory, geometry) {
+        var territotiesData = pixelArea.addBands(territory).addBands(image)
+            .reduceRegion({
+                reducer: ee.Reducer.sum().group(1, 'class').group(1, 'territory'),
+                geometry: geometry,
+                scale: scale,
+                maxPixels: 1e12
+            });
+            
+        territotiesData = ee.List(territotiesData.get('groups'));
+        var areas = territotiesData.map(convert2table);
         areas = ee.FeatureCollection(areas).flatten();
-        // store
-        recipe = recipe.merge(areas);
+        return areas;
+    };
+    
+  // perform per year 
+    var areas = bands.map(
+        function (band_i) {
+            var image = image_i.select('edge_' + distance_i + 'm_' + band_i);
+              
+            var areas = calculateArea(image, territory, geometry);
+            // set additional properties
+            areas = areas.map(
+                function (feature) {
+                    return feature.set('variable', band_i);
+                }
+            );
+            return areas;
+        }
+    );
+    
+    // apply function
+    areas = ee.FeatureCollection(areas).flatten();
+    // store
+    recipe = recipe.merge(areas);
 });
 
 // export 
 Export.table.toDrive({
       collection: recipe,
-      description: 'structural_change',
+      description: 'edge_area',
       folder: driverFolder,
       fileFormat: 'CSV'
 });

d67050d91de68191ba3feeb84a37e05c942cc394
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-02-23 16:07:07 -0300

    Update 02_plotResults.R

--- utils/benchMark/02_plotResults.R
+++ utils/benchMark/02_plotResults.R
@@ -10,10 +10,17 @@ options(scipen=9e3)
 ## read data
 data <- read.csv('./output/municipio_v1.csv')
 
-ggplot(data= data, mapping=aes(x= nome, y= total)) +
+## replace "" by NA in isolamento
+data$isolamento <- gsub('^$', NA, data$isolamento)
+
+## get parameters used
+data$layerCount <- rowSums(!is.na(data[5:10]))
+
+## plot
+ggplot(data= data, mapping=aes(x= nome, y= total, fill= as.factor(layerCount))) +
   geom_boxplot(outlier.shape= 3) +
   theme_minimal() +
   xlab(NULL) +
-  ylab('API request total time (seconds)')
+  ylab('API request total time (seconds)') +
+  labs(fill='n layers/masks')
 
-?geom_boxplot

6734d7e79e16bb0b831f67a9b464ca323934801c
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-04-03 09:01:36 -0300

    Update 05c_combineLayers.js

--- 05c_combineLayers.js
+++ 05c_combineLayers.js
@@ -26,6 +26,21 @@ var config = {
     'secondary': 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/secondary_vegetation/secondary_vegetation_age_v1',
     'classification': 'projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1'
   }
-}
+};
 
+// set years
+var yearsList = [
+  //1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
+  //2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+  //2017, 2018, 2019, 2020, 2021, 2022
+  2022
+  ];
+
+// perform combination 
+yearsList.forEach(function(year_i) {
+  // get edge
+  var edge = ee.Image(config.assets.edge).select(config.bands.edge + year_i).lte(config.params.edge).selfMask()
+  print(edge)
+  Map.addLayer(edge.randomVisualizer())
+});
 

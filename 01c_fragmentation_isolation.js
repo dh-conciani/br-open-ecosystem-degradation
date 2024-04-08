be8fcc6c913f35f9c324a13b8647f8f6fac684dc
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-04-08 11:49:30 -0300

    Update 01c_fragmentation_isolation.js

--- 01c_fragmentation_isolation.js
+++ 01c_fragmentation_isolation.js
@@ -10,9 +10,12 @@ var withWater = '7';
 
 // set isolation grid params
 var params = {
-  'mediumFrag': ['25', '50', '100'],
-  'distance': ['05', '10', '20'],
-  'bigFrag': ['100', '500', '1000'],
+  //'mediumFrag': ['25', '50', '100'],
+  'mediumFrag': ['50'],
+  //'distance': ['05', '10', '20'],
+  'distance': ['10'],
+//  'bigFrag': ['100', '500', '1000'],
+  'bigFrag': ['500'],
   'bandName' : 'nat_'
 };
 
@@ -23,9 +26,11 @@ var biomes = ee.Image('projects/mapbiomas-workspace/AUXILIAR/biomas-2019-raster'
 var biomesToApply = [3];  // only pantanal 
 
 // set years to be processed
-var yearsList = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
-                 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,
-                 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+//var yearsList = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
+//                 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014,
+//                 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
+
+var yearsList =[2022]
 
 // for each med frag
 params.mediumFrag.forEach(function(medium_i) {
@@ -35,6 +40,9 @@ params.mediumFrag.forEach(function(medium_i) {
     params.bigFrag.forEach(function(big_k) {
       
       // skip when mediumFrag and bigfrag have same values 
+      // build recipe
+      var recipe = ee.Image([]);
+      
       if(medium_i === big_k) {
         return null;
       }
@@ -52,19 +60,28 @@ params.mediumFrag.forEach(function(medium_i) {
         // for each year
         yearsList.forEach(function(year_n) {
           // get data
-          var WithoutWater_ijkmn = isolWithoutWater.select(params.bandName + year_n).aside(print)
+          var WithoutWater_ijkmn = isolWithoutWater.select(params.bandName + year_n)
+          Map.addLayer(WithoutWater_ijkmn.randomVisualizer(), {}, 'antes');
           
-        })
-        
-
-        
-        
+          // erase biome to rect 
+          WithoutWater_ijkmn = WithoutWater_ijkmn.where(biomes.eq(biomesToApply), 0).selfMask();
+          
+          // get with water data
+          var WithWater_ijkmn = isolWithWater.select(params.bandName + year_n)
+            // selet only for biome
+            .updateMask(biomes.eq(biomesToApply));
+          
+          // paste over select biome
+          var result = WithoutWater_ijkmn.blend(WithWater_ijkmn).rename(params.bandName + year_n);
+          
+          // store
+          recipe = recipe.addBands(result);
+          //Map.addLayer(WithWater_ijkmn.randomVisualizer(), {}, 'test')
+          //Map.addLayer(result.randomVisualizer(), {}, 'depois');
+        });
       });
-        
     });
   });
-  
-
-})
+});
 
 

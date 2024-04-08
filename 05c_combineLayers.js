66f5bd6a6778e6e86a17991e362576ac70a8f25e
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-04-08 17:43:16 -0300

    Update 05c_combineLayers.js

--- 05c_combineLayers.js
+++ 05c_combineLayers.js
@@ -3,6 +3,9 @@
 
 // note: fire was applied only into forest class (see lines XXX-YYY)
 
+// set output version
+var output_version = '3'; 
+
 // define params
 var config = {
   'params' : {
@@ -33,11 +36,9 @@ var config = {
 
 // set years
 var yearsList = [
-//  1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
-//  2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
-//  2017, 2018, 2019, 2020, 2021, 2022
-2022
-  //2022
+  1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000,
+  2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+  2017, 2018, 2019, 2020, 2021, 2022
   ];
 
 // perform combination 
@@ -118,8 +119,8 @@ Map.addLayer(recipe.select('frequency_2022').divide(100).round(), {palette: ['#F
 // export
 Export.image.toAsset({
 		image: recipe,
-    description: 'degradation_v2',
-    assetId: 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/summary/degradation_v2',
+    description: 'degradation_v' + output_version,
+    assetId: 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/summary/degradation_v' + output_version,
     pyramidingPolicy: 'mode',
     region: geometry,
     scale: 30,

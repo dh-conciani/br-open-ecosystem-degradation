161717a2f534605102d64f99506a5c7fff78807f
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-10-14 15:01:35 -0300

    Update 04_secondaryVegetation.js

--- 04_secondaryVegetation.js
+++ 04_secondaryVegetation.js
@@ -5,10 +5,10 @@
 var version = 1;
 
 // read coverage by mapbiomas collection 8 
-var coverage = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1');
+var coverage = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1');
 
 // read age fire by mapbiomas fogo collection 2.1
-var age = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_secondary_vegetation_age_v1');
+var age = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_secondary_vegetation_age_v1');
 
 // set native classes
 var native_classes = [3, 4, 5, 6, 11, 12, 49, 50];
@@ -20,7 +20,7 @@ var recipe = ee.Image([]);
 var years = [1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
              1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
              2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
-             2018, 2019, 2020, 2021, 2022];
+             2018, 2019, 2020, 2021, 2022, 2023];
 
 // for each year
 years.forEach(function(year_i) {
@@ -54,8 +54,8 @@ print(recipe);
 // export
 Export.image.toAsset({
 	image: recipe,
-  description: 'secondary_vegetation_age_v' + version,
-  assetId: 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/secondary_vegetation/' + 'secondary_vegetation_age_v' + version,
+  description: 'secondary_vegetation_age_col9_v' + version,
+  assetId: 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/secondary_vegetation/' + 'secondary_vegetation_age_col9_v' + version,
   region: coverage.geometry(),
   pyramidingPolicy:'mode',
   scale: 30,

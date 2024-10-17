927c061579e6f6c14efbdceedb0de368c1048727
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2024-10-17 15:58:11 -0300

    Update 06_referenceAreas.js

--- 06_referenceAreas.js
+++ 06_referenceAreas.js
@@ -11,13 +11,13 @@ var version = 1;
 var output = 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/reference_native';
 
 // read mapbiomas 
-var collection = ee.Image('projects/mapbiomas-workspace/public/collection8/mapbiomas_collection80_integration_v1');
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1');
 
 // Set years to be processed 
 var years_list = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
                   1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
                   2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
-                  2018, 2019, 2020, 2021, 2022];
+                  2018, 2019, 2020, 2021, 2022, 2023];
 
 // for each year 
 var recipe = ee.Image([]);
@@ -38,14 +38,14 @@ years_list.forEach(function(year_i) {
 });
 
 
-Map.addLayer(recipe.select('classification_2022').randomVisualizer());
+Map.addLayer(recipe.select('classification_2023').randomVisualizer());
 Map.addLayer(recipe.select('classification_1985').randomVisualizer());
 
 // export
   Export.image.toAsset({
 		image: recipe,
-    description: 'reference_v' + version,
-    assetId: output + '/' + 'reference_v' + version,
+    description: 'reference_col9_v' + version,
+    assetId: output + '/' + 'reference_col9_v' + version,
     region: collection.geometry(),
     scale: 30,
     maxPixels: 1e13

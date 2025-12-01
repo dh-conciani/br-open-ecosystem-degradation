99c8ea27096445f26ff652d3ec9fb6e3212208d8
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2025-12-01 11:37:58 -0300

    Update 06_referenceAreas.js

--- 06_referenceAreas.js
+++ 06_referenceAreas.js
@@ -1,52 +1,39 @@
-// export reference lcluc dataset 
-// dhemerson.costa@ipam.org.br
+// get native mask (reference areas)
 
-// set native classes
-var native_classes = [3, 4, 5, 6, 11, 12, 49, 50];
+// set years to be processed
+var years = [ 
+  1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+];
 
-// set version
-var version = 1;
 
-// set output 
-var output = 'projects/mapbiomas-workspace/DEGRADACAO/COLECAO/BETA/PROCESS/reference_native';
+// set collection
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_integration_v2')
 
-// read mapbiomas 
-var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection9/mapbiomas_collection90_integration_v1');
-
-// Set years to be processed 
-var years_list = [1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995,
-                  1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006,
-                  2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017,
-                  2018, 2019, 2020, 2021, 2022, 2023];
-
-// for each year 
-var recipe = ee.Image([]);
-years_list.forEach(function(year_i) {
-  // select year i
+// remap 
+var col_remap = ee.Image([]);
+years.forEach(function(year_i) {
+  
   var collection_i = collection.select('classification_' + year_i)
-    // retain only native classes
     .remap({
-      'from': native_classes,
-      'to': native_classes,
+      'from':  [3, 4, 5, 6, 11, 12, 49, 50],
+      'to':    [3, 4, 5, 6, 11, 12, 49, 50],
       'defaultValue': 0
-    })
-    .rename('classification_' + year_i)
-    .selfMask();
+    }).selfMask()
     
-    // bind
-    recipe = recipe.addBands(collection_i);
-});
+  // store
+  col_remap = col_remap.addBands(
+    collection_i.rename('classification_' + year_i));
+  
+})
 
-
-Map.addLayer(recipe.select('classification_2023').randomVisualizer());
-Map.addLayer(recipe.select('classification_1985').randomVisualizer());
-
-// export
-  Export.image.toAsset({
-		image: recipe,
-    description: 'reference_col9_v' + version,
-    assetId: output + '/' + 'reference_col9_v' + version,
+// save as gee asset
+Export.image.toAsset({
+  	image: col_remap,
+    description: 'reference_col10_v1',
+    assetId: 'projects/mapbiomas-brazil/assets/DEGRADATION/COLLECTION-10/' + 'reference_col10_v1',
     region: collection.geometry(),
     scale: 30,
-    maxPixels: 1e13
-    });
+    maxPixels: 1e13,
+    priority: 999
+  });
+

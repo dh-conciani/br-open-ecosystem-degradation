5fe8d64ffb76d33fac744d8c04ab7091bb375ff8
Dhemerson E. Conciani <dhemerson.costa@ipam.org.br>
2026-01-20 12:09:03 -0300

    Update and rename 01_getSpectra.js to 02_getSpectra.js

---
+++ invasiveSpp/02_getSpectra.js
@@ -0,0 +1,35 @@
+var samples = ee.FeatureCollection('users/dh-conciani/degradation/invasive_spp/samplesSpectra_v1')
+var year = 2024
+
+// get collection 10
+var collection = ee.Image('projects/mapbiomas-public/assets/brazil/lulc/collection10/mapbiomas_brazil_collection10_integration_v2');
+  
+
+//  load landsat mosaic
+var mosaic = ee.ImageCollection('projects/nexgenmap/MapBiomas2/LANDSAT/BRAZIL/mosaics-2')
+  .filterMetadata('biome', 'equals', 'CERRADO')
+  .filterMetadata('year', 'equals', year)
+  .mosaic()
+  .addBands(collection.select('classification_' + year));
+  
+
+// 1) Sample spectral values at point locations
+var scale = 30; // Landsat
+var samplesWithSpectra = mosaic.sampleRegions({
+  collection: samples,
+  properties: ['class_id', 'class_name'],   // keep your label column(s); add more if you need
+  scale: scale,
+  geometries: false ,           // set false if you don't want lon/lat geometry in output
+  tileScale: 8
+});
+
+print('Samples w/ spectra:', samplesWithSpectra.limit(5));
+
+// 2) Export to Google Drive as CSV
+Export.table.toDrive({
+  collection: samplesWithSpectra,
+  description: 'samples_invasiveSpecies_spectra_' + year,
+  folder: 'GEE_exports',      // change or remove if you want root
+  fileNamePrefix: 'samples_invasiveSpecies_spectra_' + year,
+  fileFormat: 'CSV'
+});
